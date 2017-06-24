52e215f9895eb4d3fd543d173f0d9fc5f8d29130
akansha <akansha.gautam3751@gmail.com>
2017-06-24 13:57:36 +0530

    Module1 Solution Coursera

--- app.js
+++ app.js
@@ -5,19 +5,21 @@ angular.module('LunchCheck',[])
 .controller('LunchCheckController' , LunchCheckController);
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
-    $scope.name="Akansha";
-    $scope.string="";
     $scope.status=0;
+    $scope.check=0;
     $scope.message=function(){
       var s=splitString($scope.string,',')
       $scope.status=s;
-    }
+    };
     function splitString(stringToSplit, separator){
       var arrayOfStrings = stringToSplit.split(separator);
       var length=arrayOfStrings.length;
       return length;
 
-    }
+    };
+    $scope.chk= function (){
+      $scope.check=1;
+    };
   }
 })();
 
